d46ddbaa67d2834097c57c646ae3f862b7fc6416
FullMetalPr0gr1mmer <Kareem.hosny2001@gmail.com>
2025-06-25 14:06:28 +0300

    Initialization

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from extractor.skill_extractor import extract_skills
-
+import streamlit as st
 sample_resume = """
 Experienced software engineer with expertise in Python, machine learning, and backend development.
 Worked with TensorFlow, Flask, and AWS. Built scalable APIs and NLP tools.
 """
-
+st.subheader("Hello")
 print(extract_skills(sample_resume))
